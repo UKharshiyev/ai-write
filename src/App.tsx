f9db7c9681200241d3937dd6bf8c51ff20e4731f
Qarshiyev <ulugbek.kharshiyev@mail.ru>
2025-04-17 23:08:40 +1200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button variant="link">Click me</Button>
+    </div>
+  )
+}
+
+export default App
