bf3bf3c9e825c92ea3da77a2ba818f3d863f0ba6
Qarshiyev <ulugbek.kharshiyev@mail.ru>
2025-05-23 21:52:27 +1200

    Created Main page

---
+++ src/components/dashboard/ContentCreate.tsx
@@ -0,0 +1,12 @@
+export default function ContenCreate() {
+    return (
+        <div>
+            <h1 className="text-3xl font-semibold">
+                Article write
+            </h1>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
